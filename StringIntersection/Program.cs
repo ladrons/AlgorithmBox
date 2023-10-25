2e0036ae9e79243dbc0198f538d52d3bc8a40286
ladrons <burak_cevik76@hotmail.com.tr>
2023-10-25 18:54:18 +0300

    Added String Intersection algorithm.

---
+++ StringIntersection/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("İşlem yapılacak sayılar: [1, 3, 4, 7, 13] - [1, 2, 4, 13, 15]");
+Console.WriteLine("");
+
+string[] strArr = { "1, 3, 4, 7, 13", "1, 2, 4, 13, 15" };
+
+string result = string.Join(",", strArr[0].Split(',').Select(x => x.Trim()).Intersect(strArr[1].Split(',').Select(x => x.Trim())));
+
+if (!string.IsNullOrEmpty(result))
+{
+    Console.WriteLine($"Eşleşen değerler: {result}");
+}
+else { Console.WriteLine("Eşleşen değer yoktur."); }
