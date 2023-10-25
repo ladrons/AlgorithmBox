e69ea79d7c8f20c29c9d2d5d55d6ba9a85f8df10
ladrons <burak_cevik76@hotmail.com.tr>
2023-10-25 15:26:00 +0300

    2 new algorithms have been added.

---
+++ CalculateFactorial/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Lütfen faktöriyelini hesaplamak istediğiniz pozitif bir tam sayı girin: ");
+Console.WriteLine(" ");
+
+int value = Convert.ToInt32(Console.ReadLine());
+
+int factorial = 1;
+for (int i = 1; i <= value; i++)
+{
+    factorial *= i;
+}
+
+Console.WriteLine($"{value} sayısının faktöriyeli: {factorial}");
---
+++ ReverseString/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Ters çevirilecek bir metin girin: ");
+Console.WriteLine("");
+
+string original = Console.ReadLine();
+string reversed = ReverseString(original);
+
+Console.WriteLine($"Metnin ters hali: {reversed}");
+
+
+static string ReverseString(string input)
+{
+    char[] charArray = input.ToCharArray();
+    Array.Reverse(charArray);
+
+    return new string(charArray);
+}
+
+
+////Farklı Yöntem (LINQ) ////
+
+//string original = Console.ReadLine();
+//string reversed = new string(original.Reverse().ToArray());
+
+//Console.WriteLine($"Metnin ters hali: {reversed}");
